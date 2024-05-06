2accbe95826f9403f42f03f37f42abe86728b3c5
joaquim6399 <joaquim6399@gmail.com>
2024-05-06 21:33:26 +0100

    Add Person class to project

---
+++ MyFamilyMembers/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFamilyMembers
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Relation { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+
+    }
+}
