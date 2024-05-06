6a4d9735e4b5c7e974bd695b760f4cc146e786eb
joaquim6399 <joaquim6399@gmail.com>
2024-05-06 21:48:51 +0100

    Add list with family members and endpoint to mum

--- MyFamilyMembers/Program.cs
+++ MyFamilyMembers/Program.cs
@@ -3,8 +3,15 @@ using MyFamilyMembers;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var familyMembers = new List<Person>
+{
+    new Person("Josefa", "Mum"),
+    new Person("Raja", "Girlfriend")
+};
+
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/joaquim", () => new Person("Joaquim", "Me") { Age = 26, Sex = "Male"});
+app.MapGet("/joaquim/mum", () => familyMembers.Where(u => u.Relation == "Mum"));
 
 
 app.Run();
