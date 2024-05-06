1c655cde58f02a59b41f304a71abfd8a87f04444
joaquim6399 <joaquim6399@gmail.com>
2024-05-06 21:39:47 +0100

    Add endpoint for me url

--- MyFamilyMembers/Program.cs
+++ MyFamilyMembers/Program.cs
@@ -1,6 +1,11 @@
+using MyFamilyMembers;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/me", () => new Person { Name = "Joaquim", Age = 26, Relation = "Me", Sex = "Male"});
+
 
 app.Run();
