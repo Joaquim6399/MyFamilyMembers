9724c1f48bc0fb5dc44383a2fea7b2bceb98dac9
Joaquim Soares <joaquimsoares@Joaquims-MacBook-Pro.local>
2024-05-08 19:10:26 +0100

    Remove unnecessary properties from person class
    As we will use a adjency matrix to represent a graph that will represent the social connections between my people

--- MyFamilyMembers/Person.cs
+++ MyFamilyMembers/Person.cs
@@ -3,16 +3,12 @@
     public class Person
     {
         public string Name { get; set; }
-        public string Relation { get; set; }
         public string Sex { get; set; }
         public int Age { get; set; }
 
-        public List<Person> FamilyMembers { get; set; }
-
-        public Person(string name, string relation)
+        public Person(string name)
         {
-            Name = name;
-            Relation = relation;
+            Name = name.ToLower();
         }
     }
 }
